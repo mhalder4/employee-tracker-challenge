b9db2e80b4db75c38eedfd2bf42a1c5fcd5d5fc4
unknown <mhalder9555@gmail.com>
2023-12-04 02:07:40 -0600

    Fixed db creation problems.

--- db/schema.sql
+++ db/schema.sql
@@ -3,12 +3,12 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department{
+CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(30)
-};
+);
 
-CREATE TABLE role{
+CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
@@ -16,9 +16,9 @@ CREATE TABLE role{
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
-};
+);
 
-CREATE TABLE employee{
+CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
@@ -26,9 +26,9 @@ CREATE TABLE employee{
   manager_id INT,
   FOREIGN KEY (role_id)
   REFERENCES role(id)
-  ON DELETE SET NULL
+  ON DELETE SET NULL,
   FOREIGN KEY (manager_id)
   REFERENCES employee(id)
   ON DELETE SET NULL
 
-};
+);
